3184a496ea3cbb955478d59dc7df775d731e8828
chenxinghua <402326588@qq.com>
2017-07-17 22:45:33 +0800

    company.java

--- src/main/java/com/pcms/model/Company.java
+++ src/main/java/com/pcms/model/Company.java
@@ -8,10 +8,22 @@ public class Company implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 467925414222557520L;
+	
+	private Long id;
 
 	private String companyName;
 	
 	private String companyCode;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getCompanyName() {
 		return companyName;
